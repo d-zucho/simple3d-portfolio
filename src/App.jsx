23e516def78a6eac3851c3d76300094fc6bdc49f
d-zucho <danny.nico.dev@outlook.com>
2025-07-30 16:52:39 -0700

    initial + header

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from './sections/Hero'
+import Navbar from './sections/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/HeroText.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HeroText = () => {
+  return (
+    <div className='z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text'></div>
+  )
+}
+
+export default HeroText
